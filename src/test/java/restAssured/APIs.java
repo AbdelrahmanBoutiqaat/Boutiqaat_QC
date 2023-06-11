667b7ba6c23e55d5d8b84c3fbfffbdb689478848
Abdelrahman <Abdelrahman@DESKTOP-RNO8TUS>
2023-06-11 15:35:39 +0300

    new requests

--- src/test/java/restAssured/APIs.java
+++ src/test/java/restAssured/APIs.java
@@ -2,14 +2,9 @@ package restAssured;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-
 import static org.testng.Assert.assertEquals;
-
-import javax.security.auth.callback.ConfirmationCallback;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 
 @Test
@@ -25,7 +20,13 @@ public class APIs {
 	String Confirm = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
 	String Complete = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
 	String ViewList = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
-	String BINDataFromNAV="https://staging-wms.boutiqaat.com/api/WMS/BINDataFromNAV";
+	String BINDataFromNAV = "https://staging-wms.boutiqaat.com/api/WMS/BINDataFromNAV";
+	String MVMNT_Pick_Lot_On_Load = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
+	String On_barcode_Scan = "https://staging-wms.boutiqaat.com/api/WMS/BINDataFromNAV";
+	String OnConfirm = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
+	String OnPost = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
+	String VieWlist = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
+	String DocList = "https://staging-wms.boutiqaat.com/api/WMS/Movement";
 	String TokenID;
 
 	@BeforeClass
@@ -39,22 +40,24 @@ public class APIs {
 		TokenID = jsonpath.get("result.TOCKEN").toString().replace("[", "").replace("]", "");
 		System.out.println("Token ID =" + TokenID);
 		assertEquals(UserID, "[12511]");
+		System.out.println("--------------------------------------------------------------");
 
 	}
-
-	// Login
-	@Test(priority = 1)
-	public void Login() {
-		String Body = "{\r\n" + "    \"userNM\": \"12511\",\r\n" + "    \"userPWD\": \"wms@1230\",\r\n"
-				+ "    \"DeviceID\": \"RT050\",\r\n" + "    \"Tag\": 1\r\n" + "}";
-		Response response = given().contentType("application/json").body(Body).post(login);
-		System.out.println(response.getBody().asString());
-		JsonPath jsonpath = response.jsonPath();
-		String UserID = jsonpath.get("result.USERID").toString();
-		String TokenID2 = jsonpath.get("result.TOCKEN").toString();
-		System.out.println("Token ID =" + TokenID2);
-		assertEquals(UserID, "[12511]");
-	}
+	/* ---------------------------Login Screen------------------- */
+	/*
+	 * // Login
+	 * 
+	 * @Test(priority = 1) public void Login() { String Body = "{\r\n" +
+	 * "    \"userNM\": \"12511\",\r\n" + "    \"userPWD\": \"wms@1230\",\r\n" +
+	 * "    \"DeviceID\": \"RT050\",\r\n" + "    \"Tag\": 1\r\n" + "}"; Response
+	 * response = given().contentType("application/json").body(Body).post(login);
+	 * System.out.println(response.getBody().asString()); JsonPath jsonpath =
+	 * response.jsonPath(); String UserID =
+	 * jsonpath.get("result.USERID").toString(); String TokenID2 =
+	 * jsonpath.get("result.TOCKEN").toString(); System.out.println("Token ID =" +
+	 * TokenID2); assertEquals(UserID, "[12511]"); System.out.println(
+	 * "--------------------------------------------------------------"); }
+	 */
 
 	// HOME PAGE COUNTS
 	@Test(priority = 2)
@@ -67,8 +70,10 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(homePage_Counts);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
+	/*---------------------------Support Screen -------------------------------*/
 	// MUTI-IMAGE ENQUIRY
 	@Test(priority = 3)
 	public void MultiImage_Inquiry() {
@@ -78,8 +83,10 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(MultiImage_Inquiry);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
+	/*-------------------------------Others Screen------------------------------*/
 	// ITEMS EXPIRY : ON BARCODE SCAN - 1
 	@Test(priority = 4)
 	public void Item_Expiry_Scan1() {
@@ -89,6 +96,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(Item_Expiry_Scan1);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 
 	}
 
@@ -101,6 +109,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(Item_Expiry_Scan2);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 
 	}
 
@@ -116,6 +125,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(ItemExipryInfo);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 
 	}
 
@@ -128,8 +138,10 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(GetItemDimensionDetail);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
+	/*------------------------------------Movement Pick Single Lot --------------------------*/
 	// ONBINSCAN
 	@Test(priority = 7)
 	public void ONBINSCAN() {
@@ -139,6 +151,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(ONBINSCAN);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
 	// Confirm
@@ -150,6 +163,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(Confirm);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
 	// Complete -- Not valid data response
@@ -161,6 +175,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(Complete);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
 	// ViewList
@@ -172,6 +187,7 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(ViewList);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
 	// ON BARCODE SCAN
@@ -183,6 +199,202 @@ public class APIs {
 		System.out.println(RequestBody);
 		Response response2 = given().contentType("application/json").body(RequestBody).post(BINDataFromNAV);
 		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
 	}
 
+	/*-------------------------Movement Pick Lot Wise screen--------------------------*/
+	@Test(priority = 12)
+	public void onLoad() {
+		String RequestBody = "{\r\n" + "    \"Status\": \"0\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "\"TokenID\":\"" + TokenID + "\"" + "," + "    \"DeviceID\": \"01D86\",\r\n"
+				+ "    \"WHNO\": \"KWI01\",\r\n" + "    \"DOCTYPE\": \"600\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(MVMNT_Pick_Lot_On_Load);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+
+	}
+
+	@Test(priority = 13)
+	public void ON_BARCODE_SCAN() {
+		String RequestBody = "{\r\n" + "    \"BinNo\": \"KWI19-G007-R015-01A1\",\r\n" + "\"TokenID\":\"" + TokenID
+				+ "\"" + "," + "    \"WHNO\": \"KWI01\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "    \"NavBaseUrl\": \"https://staging-omsapi.boutiqaat.com\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(On_barcode_Scan);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 14)
+	public void OnConfirm() {
+		String RequestBody = "{\r\n" + "    \"Status\": \"1\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "\"TokenID\":\"" + TokenID + "\"" + "," + "    \"BinNo\": \"KWI19-G007-R015-01A1\",\r\n"
+				+ "    \"Barcode\": \"SC-00006810_LOT00055559\",\r\n" + "    \"ItemNo\": \"SC-00006810\",\r\n"
+				+ "    \"ItemDesc\": \"\",\r\n" + "    \"LOTNO\": \"LOT00055559\",\r\n" + "    \"WHNO\": \"KWI01\",\r\n"
+				+ "    \"DOCNO\": \"\",\r\n" + "    \"Qty\": \"1\",\r\n" + "    \"DOCTYPE\": \"600\",\r\n"
+				+ "    \"STAGEBINNO\": \"STAGEMOVEBIN\",\r\n" + "    \"SYSBINNO\": \"KWI19-G009-L017-05A1\",\r\n"
+				+ "    \"SYSZONEID\": \"ZONE16\",\r\n" + "    \"BINNO_1\": \"ZONE14  KWI19-G007-R014-05A1\",\r\n"
+				+ "    \"BINNO_2\": \"ZONE08  KWI01-G024-R001-01A1\",\r\n"
+				+ "    \"BINNO_3\": \"G038  KWI01-G038-Y001-01A3\",\r\n" + "    \"BINNO_4\": \"\",\r\n"
+				+ "    \"DeviceID\": \"01D90\",\r\n" + "    \"Misc\": \"0\",\r\n" + "    \"SYSQTY\": \"10\",\r\n"
+				+ "    \"Tag\":\"\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(OnConfirm);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 15)
+	public void OnPost() {
+		String RequestBody = "{\r\n" + "    \"Status\": \"2\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "    \"DOCNO\": \"BL/2223/000000021928\",\r\n" + "\"TokenID\":\"" + TokenID + "\"" + ","
+				+ "    \"DeviceID\": \"01D90\",\r\n" + "    \"WHNO\": \"KWI01\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(OnPost);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 16)
+	public void ViewList() {
+		String RequestBody = "{\r\n" + "    \"Status\": \"14\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "\"TokenID\":\"" + TokenID + "\"" + "," + "    \"DOCNO\": \"BL/2223/000000021928\",\r\n"
+				+ "    \"WHNO\": \"KWI01\",\r\n" + "    \"DeviceID\": \"01D90\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(VieWlist);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	/*----------------------------MOVEMENT SINGLE LOT PUT Screen---------------------*/
+	@Test(priority = 17)
+	public void DOCLIST() {
+		String RequestBody = "{\r\n" + "\"Status\": \"3\",\r\n" + "\"userID\": \"12511\",\r\n"
+				+ "\"ItemType\": \"2\",\r\n" + "\"TokenID\":\"" + TokenID + "\"" + "," + "\"WHNO\": \"KWI01\",\r\n"
+				+ "\"DeviceID\": \"01D90\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 18)
+	public void Page_ON_Load() {
+		String RequestBody = "{\r\n" + "\"Status\": \"4\",\r\n" + "\"userID\": \"12511\",\r\n" + "\"TokenID\":\""
+				+ TokenID + "\"" + "," + "\"DOCNO\": \"MS/2223/000000003057\",\r\n" + "\"WHNO\": \"KWI01\",\r\n"
+				+ "\"DeviceID\": \"01D90\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 19)
+	public void BARCODE_SCAN() {
+		String RequestBody = "{\r\n" + "\"Status\":\"5\",\r\n" + "\"userID\":\"12511\",\r\n" + "\"TokenID\":\""
+				+ TokenID + "\"" + "," + "\"DOCNO\":\"MS/2223/000000003058\",\r\n" + "\"Barcode\": \"100995_BLOT\",\r\n"
+				+ "\"WHNO\":\"KWI01\",\r\n" + "\"DeviceID\":\"01D90\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 20)
+	public void ConFirm() {
+		String RequestBody = "{\r\n" + "    \"Status\": \"6\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "\"TokenID\":\"" + TokenID + "\"" + "," + "    \"DOCNO\": \"MS/2223/000000003059\",\r\n"
+				+ "    \"BinNo\": \"KWI19-G013-L011-03A1\",\r\n" + "    \"Barcode\": \"100995_BLOT\",\r\n"
+				+ "    \"ItemNo\": \"\",\r\n" + "    \"Qty\": \"1\",\r\n" + "    \"WHNO\": \"KWI01\",\r\n"
+				+ "    \"ItemType\": \"UPK\",\r\n" + "    \"DeviceID\": \"01D90\",\r\n" + "    \"Misc\": \"0\"\r\n"
+				+ "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+	@Test(priority = 21)
+	public void ViEwList() {
+		String RequestBody = "{\r\n"
+				+ "\"status\": \"7\",\r\n"
+				+ "\"DOCNO\": \"MS/2223/000000003059\",\r\n"
+				+ "\"userID\":\"12511\",\r\n"
+				+"\"TokenID\":\"" + TokenID + "\"" + ","
+				+ "\"Tag\": \"0\",\r\n"
+				+ "\"WHNO\": \"KWI01\",\r\n"
+				+ "\"DeviceID\": \"01D90\"\r\n"
+				+ "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+	/*--------------------------------Movement Put Lot Wise-------------------------------*/
+	@Test(priority = 21)
+	public void DOCLiST() {
+		String RequestBody = "{\r\n"
+				+ "    \"Status\": \"3\",\r\n"
+				+ "    \"userID\": \"12511\",\r\n"
+				+ "    \"ItemType\": \"2\",\r\n"
+				+ "\"TokenID\":\"" + TokenID + "\"" + ","
+				+ "    \"WHNO\": \"KWI01\",\r\n"
+				+ "    \"DeviceID\": \"01D90\"\r\n"
+				+ "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+	@Test(priority = 22)
+	public void Page_ON_load() {
+		String RequestBody = "{\r\n" + "\"Status\": \"4\",\r\n" + "\"userID\": \"12511\",\r\n" + "\"TokenID\":\""
+				+ TokenID + "\"" + "," + "\"DOCNO\": \"MS/2223/000000003057\",\r\n" + "\"WHNO\": \"KWI01\",\r\n"
+				+ "\"DeviceID\": \"01D90\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 23)
+	public void BARCODE_sCAN() {
+		String RequestBody = "{\r\n" + "\"Status\":\"5\",\r\n" + "\"userID\":\"12511\",\r\n" + "\"TokenID\":\""
+				+ TokenID + "\"" + "," + "\"DOCNO\":\"MS/2223/000000003058\",\r\n" + "\"Barcode\": \"100995_BLOT\",\r\n"
+				+ "\"WHNO\":\"KWI01\",\r\n" + "\"DeviceID\":\"01D90\"\r\n" + "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	@Test(priority = 24)
+	public void ConfIrm() {
+		String RequestBody = "{\r\n" + "    \"Status\": \"6\",\r\n" + "    \"userID\": \"12511\",\r\n"
+				+ "\"TokenID\":\"" + TokenID + "\"" + "," + "    \"DOCNO\": \"MS/2223/000000003059\",\r\n"
+				+ "    \"BinNo\": \"KWI19-G013-L011-03A1\",\r\n" + "    \"Barcode\": \"100995_BLOT\",\r\n"
+				+ "    \"ItemNo\": \"\",\r\n" + "    \"Qty\": \"1\",\r\n" + "    \"WHNO\": \"KWI01\",\r\n"
+				+ "    \"ItemType\": \"UPK\",\r\n" + "    \"DeviceID\": \"01D90\",\r\n" + "    \"Misc\": \"0\"\r\n"
+				+ "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
+	@Test(priority = 25)
+	public void ViEwLIst() {
+		String RequestBody = "{\r\n"
+				+ "\"status\": \"7\",\r\n"
+				+ "\"DOCNO\": \"MS/2223/000000003059\",\r\n"
+				+ "\"userID\":\"12511\",\r\n"
+				+"\"TokenID\":\"" + TokenID + "\"" + ","
+				+ "\"Tag\": \"0\",\r\n"
+				+ "\"WHNO\": \"KWI01\",\r\n"
+				+ "\"DeviceID\": \"01D90\"\r\n"
+				+ "}";
+		System.out.println(RequestBody);
+		Response response2 = given().contentType("application/json").body(RequestBody).post(DocList);
+		System.out.println(response2.getBody().asString());
+		System.out.println("--------------------------------------------------------------");
+	}
 }
